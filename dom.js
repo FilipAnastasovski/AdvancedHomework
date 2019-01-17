1c5f3e2be13ce1bd6bba7fa44f7192e029fe8831
FilipAnastasovski <45153380+FilipAnastasovski@users.noreply.github.com>
2019-01-17 09:58:38 +0100

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,29 +1,67 @@
 let plorp = ["aaaaa", "bbbbbbb", "cccccccc", "ddddddddd", "ee", "ffffff", "ggggggggggggg", "asdfghjjjreereeq"];
 
-let longest = plorp.reduce(function(a, b) { 
+function find(example){
 
-  return a.length > b.length ? a : b
-}, '');
+    let found;
+    let length = 0;
+    let words = example.split(" ");
 
-console.log(longest);
+    for( let i=0;  i < words.length;  i++){
+
+        if(words[i].length > length){
+            length = words[i].length;
+            found = words[i];
+        }
+    }
+    return found;
+}
+
+console.log( find("what is the longest word aaaaaaaa bbbbbbbb ccccccccc ggg eeeeeeeeeee"));
 
 /////////////////
 
-let tree = function(leafs) {
+let result = [];
+let input;
+function combine(primer) {
+  
+    let allElements = primer.split("");
+    let char = "";
+    
+    for (let i = 0; i < allElements.length - 1; i++) {
+      
+        char += allElements[i];
 
-    let branches = [];      
-
-    if( leafs.length == 1 ) return leafs;       
-
-    for( let k in leafs ) {
-
-        let leaf = leafs[k];
-
-        tree(leafs.join('').replace(leaf,'').split('')).concat("").map(function(subtree) {
-
-            branches.push([leaf].concat(subtree));
+        for (let j = (i + 1); j < allElements.length; j++) {
+          
+            let resultTopush = char + allElements[j];
+            result.push(resultTopush);
+        }
+    }
+    allElements.shift();
+    let pomoshno = allElements.join("");
+    
+    if (allElements.length !== 0) {
+        combine(pomoshno);
+    }
+    
+    else {
+        let char = input.split("");
+        char.forEach(element => {
+            result.push(element)
         });
     }
-    return branches;
-};
-console.log(tree("abcd".split('')).map(function(str){return str.join('')}))
+}
+
+combine(input = "abc");
+console.log(result);
+
+/////
+
+function reverse_a_number(n)
+{
+	n = n + "";
+	return n.split("").reverse().join("");
+}
+
+console.log(reverse_a_number(10101010));
+console.log(reverse_a_number(54321));
